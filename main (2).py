c77a115b519eadc46c64f3f66de20e381495b79b
heraherlinaputri12345678 <heraherlinaputri65@gmail.com>
2026-04-02 09:00:21 +0700

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,2 @@
+print("Hello, World!")
+print("Hello");print("hai");print("salam kenal");print("salam hangat");print("dari");print("kami");print("senang");print("bertemu dengan mu")
---
+++ main.py
@@ -0,0 +1,6 @@
+Kelas="Xll"
+RPL=3
+umur=18
+
+print("halo,nama saya yolanda dari kelas")
+print("Xll RPL3 umur 18 tahun ") 
